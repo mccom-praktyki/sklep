078206a4c2b62b782cd0938a2352d68461fc5458
MateuszWeiser <m.weiser87@gmail.com>
2016-05-13 14:50:31 +0200

    Merge branch 'master' of https://github.com/mccom-praktyki/sklep
    # Conflicts:
    #	js/acp.js

--- includes/acp/dodawanie_kategorii.php
+++ includes/acp/dodawanie_kategorii.php
@@ -6,14 +6,16 @@ $_SESSION['Administrator'] = 1;
 if ($_SESSION['Zalogowany']==1 and $_SESSION['Administrator']==1) {
 ?>
 
-<form method="POST" action="">
-    <div style="margin-top:30px; width:700px; margin-left:40px;" class="alert alert-info"><div class="glyphicon glyphicon-info-sign"></div>  Aby dodać kategorię, wpisz jej nazwe i kliknij przycisk poniżej pola tekstowego.</div>
-    <div class="form-group" style="margin:30px;">
-        <label>Nazwa kategorii</label>
-        <input type="text" class="form-control" placeholder="Wpisz nazwę kategorii" style="width:300px;">
-    </div>
-    <button type="submit" style="float:right; margin-right: 40px; margin-top:20px;" class="btn btn-success">Dodaj kategorię</button>
-</form>
+    <form method="POST" action="">
+        <div style="margin-top:30px; width:700px; margin-left:40px;" class="alert alert-info"><div class="glyphicon glyphicon-info-sign"></div>  Aby dodać kategorię, wpisz jej nazwe i kliknij przycisk poniżej pola tekstowego.</div>
+        <div class="form-group" style="margin:30px;">
+            <label>Nazwa kategorii</label>
+            <table>
+                <tr><td>
+
+                        <input type="text" class="form-control" placeholder="Wpisz nazwę kategorii" style="width:300px;"></td>
+                    <td>
+                        <button type="submit" style="margin-left:10px;" class="btn btn-success">Dodaj kategorię</button></div></td></tr></table>
 
 <?php
 } else {
