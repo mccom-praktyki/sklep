916175e0cda846d693cbdfd70f323ffc1f1263ba
MateuszWeiser <m.weiser87@gmail.com>
2016-05-13 14:25:28 +0200

    Merge branch 'master' of https://github.com/mccom-praktyki/sklep
    # Conflicts:
    #	js/acp.js

--- js/produkty_slide.js
+++ js/produkty_slide.js
@@ -1,6 +1,8 @@
-function produkty(id) {
+$(document).ready(function(){
     $("#slidedown4").show();
     $("#slidedown6").hide();
     $("#slidedown8").hide();
+});
+function produkty(id){
     $("#"+id+"").addClass("active");
 }
