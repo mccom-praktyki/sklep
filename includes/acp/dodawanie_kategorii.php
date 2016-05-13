3ed34fdf6780d651ccf96afbd454144dc1371ef8
MateuszWeiser <m.weiser87@gmail.com>
2016-05-13 14:40:27 +0200

    Merge branch 'master' of https://github.com/mccom-praktyki/sklep
    # Conflicts:
    #	js/acp.js

--- includes/acp/dodawanie_kategorii.php
+++ includes/acp/dodawanie_kategorii.php
@@ -7,7 +7,12 @@ if ($_SESSION['Zalogowany']==1 and $_SESSION['Administrator']==1) {
 ?>
 
 <form method="POST" action="">
-
+    <div style="margin-top:30px; width:400px;" class="alert alert-info"><div class="glyphicon glyphicon-info-sign"></div>  Aby dodać kategorię, wpisz jej nazwe i kliknij przycisk poniżej pola tekstowego.</div>
+    <div class="form-group" style="margin:30px;">
+        <label>Nazwa kategorii</label>
+        <input type="text" class="form-control" placeholder="Wpisz nazwę kategorii" style="width:300px;">
+    </div>
+    <button type="submit" style="float:right; margin-right: 40px; margin-top:20px;" class="btn btn-success">Dodaj kategorię</button>
 </form>
 
 <?php
