3c8f3b8cbeabaeffc9fc7934ab61a1fb409e74ba
Krzysztof Szymanski <krzysiek79855@wp.pl>
2016-05-16 10:16:03 +0200

    Aktualizacja

--- includes/acp/dodawanie_produktow_ajax.php
+++ includes/acp/dodawanie_produktow_ajax.php
@@ -25,16 +25,23 @@ return mysql_query($query);
 
 	$id_kat = $_POST['kategoria'];
 	$cechy = kwerenda("SELECT id,nazwa FROM cechy WHERE id_kategorii=$id_kat");
+	$kategorie_razem = kwerenda("SELECT id,nazwa FROM kategorie");
 	
 
 
-		$id = array(); $nazwa = array();
+		$id = array(); $nazwa = array(); 
+		$id_razem = array(); $nazwa_razem = array();
 		while($row = mysql_fetch_array($cechy)) {
         $id[] = $row["id"]; 
         $nazwa[] = $row["nazwa"];
 		}
+		while($row2 = mysql_fetch_array($kategorie_razem)) {
+        $id_razem[] = $row2["id"]; 
+        $nazwa_razem[] = $row2["nazwa"];
+		}
 		$ilosc[] = mysql_num_rows($cechy);
-		$res = array($id, $nazwa, $ilosc);
+		$ilosc_razem[] = mysql_num_rows($kategorie_razem);
+		$res = array($id, $nazwa, $ilosc, $id_razem, $nazwa_razem, $ilosc_razem);
 		echo json_encode($res);
 
  
