f299a20942ec957bd386ada29933913e799a82a8
MateuszWeiser <m.weiser87@gmail.com>
2016-05-16 12:00:07 +0200

    Merge branch 'master' of https://github.com/mccom-praktyki/sklep
    # Conflicts:
    #	js/acp.js

--- includes/acp/dodawanie_kategorii.php
+++ includes/acp/dodawanie_kategorii.php
@@ -34,8 +34,21 @@ if ($_SESSION['Zalogowany']==1 and $_SESSION['Administrator']==1) {
                     <td>
                         <button type="submit" style="float:right; margin-left:10px;" class="btn btn-danger">Usuń</button></td></tr></table></div>
         <hr>
-        <div style="margin-top:30px; width:500px; margin-left:40px;" class="alert alert-info"><div class="glyphicon glyphicon-info-sign"></div>  Aby edytować kategorię, kliknij na "dłogupis" znajdujący sie przy nazwie.</div>
+
+        <hr>
+        <div style="margin-top:30px; width:550px; margin-left:40px;" class="alert alert-info"><div class="glyphicon glyphicon-info-sign"></div> Aby edytować kategorię wybierz ją i kliknij przycisk "edytuj".</div>
         <div class="form-group" style="margin:30px;">
+            <label for="sel1">Wybierz kategorię do edycji:</label>
+            <table>
+                <tr><td>
+                        <select style="width:200px;" class="form-control">
+                            <option>kat1</option>
+                            <option>kat2</option>
+                            <option>kat3</option>
+                            <option>kat4</option>
+                        </select></td>
+                    <td>
+                        <button type="submit" style="float:right; margin-left:10px;" class="btn btn-warning">Edytuj</button></td></tr></table></div>>
 <?php
 } else {
 header("strona glowna");
