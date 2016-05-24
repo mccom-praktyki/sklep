70608b93ef1874d2e3395225f9a57718e0dbcddf
MateuszWeiser <m.weiser97@gmail.com>
2016-05-24 11:03:59 +0200

    Strona główna + skrypty + owlCarousel.

--- js/produkty_carousel.js
+++ js/produkty_carousel.js
@@ -1,13 +1,22 @@
- $(document).ready(function() {
+$(document).ready(function() {
+	$("#owl-demo").owlCarousel({
+		navigation : true, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		items : 1,
+		itemsDesktop : false,
+		itemsDesktopSmall : false,
+		itemsTablet: false,
+		itemsMobile : false,
+		autoplay : true,
+		autoplayTimeout: 5000,
+		loop : true
+	});
+});
+$(document).ready(function() {
 	$(".kategorie").owlCarousel({
 		nav : true,
 		navText : [btnleft, btnright],
-		loop : false,
+		loop : true,
 	});
-  });
-  $(document).ready(function(){
-    $(".btn1").click(function(){
-		$( "#ukrycie").stop(true, false).slideToggle();
-
-    });
 });
